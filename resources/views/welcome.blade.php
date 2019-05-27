37b20b5f4dedb1a8fe66bebe7b9e0c96c7e25519
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-05-27 16:43:47 +0530

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+
+<html>
+  <head>
+     <link rel="stylesheet" type="text/css" href="css/P3.css">
+  </head>
+  <body>
+    <p> PLAY </p>
+    <hr> </hr>
+    <form method="post" action="">
+      <input id="user" type="text" name="user" />
+      <div id="use"> </div>
+      <input id="comp" type="text" name="comp" />
+      <div id="com"></div>
+      <input type="image" id="mid" src="mid.png" />
+      <input id="sub" type="submit" value="click" />
+
+      <input type="image" id="pass" src="left.png" />
+      <input type="image" id="top" src="back.png" />
+    </form>
+  </body>
+</html>
