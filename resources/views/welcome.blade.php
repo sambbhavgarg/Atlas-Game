83a2c7c7de1ebd101e75c3c1a8ae804ae39f3924
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-06-01 03:23:00 +0530

    It's a little bit of a mess.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
-
-<html>
+<html lang="en" dir="ltr">
   <head>
+    <meta charset="utf-8">
      <link rel="shortcut icon" type="image/png" href="/globe-icon.png"/>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
      <link rel="stylesheet" type="text/css" href="css/P3.css"/>
      <title>Play!</title>
@@ -11,20 +12,17 @@
     <hr> </hr>
     <form method="POST" action="/userentry">
       {{ csrf_field() }}
-      <input id="user" type="text" name="Input" class="input {{ $errors->has('entered') ? 'is-danger' : '' }}" placeholder="Enter Country or Capital" value="{{ old('entered') }}"/>
-
+      <input id="user" type="text" name="Input"
+              class="input {{ $errors->has('entered') ? 'is-danger' : '' }}"
+              placeholder="Enter Country or Capital"
+              value="{{ old('entered') }}"
+      />
       <div id="next2user"> </div>
-
       <input id="comp" type="text" name="compdisplay" />
-
       <div id="next2comp"></div>
-
       <input type="image" id="mid" src="mid.png" />
-
       <input id="sub" type="submit" value="click" />
-
       <input type="image" id="pass" src="left.png" />
-
       <input type="image" id="top" src="back.png" />
 
       @if ($errors->any())
@@ -36,7 +34,6 @@
         </ul>
       </div>
       @endif
-
     </form>
   </body>
 </html>
